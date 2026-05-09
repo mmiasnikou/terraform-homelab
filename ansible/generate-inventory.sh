3d8cdbba40ded5e139db16489dc307e0172dad0f
Mikhail Myasnikov <miasnikou.mikhail@gmail.com>
2026-05-09 16:12:24 +0000

    Step 16: k3s cluster (1 cp + 2 workers), kubectl on lab-control

--- ansible/generate-inventory.sh
+++ ansible/generate-inventory.sh
@@ -3,13 +3,12 @@ set -e
 
 cd ../proxmox
 terraform output -json vm_ips | jq -r '
-  "[lab_vms]\n" +
-  ([to_entries[] | "\(.key) ansible_host=\(.value)"] | join("\n")) +
-  "\n\n[forgejo]\n" +
-  ([to_entries[] | select(.key == "web-01") | "\(.key) ansible_host=\(.value)"] | join("\n")) +
-  "\n\n[webservers]\n" +
-  ([to_entries[] | select(.key == "web-02") | "\(.key) ansible_host=\(.value)"] | join("\n")) +
-  "\n\n[lab_vms:vars]\n" +
+  "[k3s_cluster:children]\nk3s_cp\nk3s_workers\n\n" +
+  "[k3s_cp]\n" +
+  ([to_entries[] | select(.key == "k3s-cp") | "\(.key) ansible_host=\(.value)"] | join("\n")) +
+  "\n\n[k3s_workers]\n" +
+  ([to_entries[] | select(.key | startswith("k3s-w")) | "\(.key) ansible_host=\(.value)"] | join("\n")) +
+  "\n\n[k3s_cluster:vars]\n" +
   "ansible_user=mik\n" +
   "ansible_ssh_private_key_file=~/.ssh/id_ed25519\n" +
   "ansible_python_interpreter=/usr/bin/python3"
